2919a6d1fecb23a70280e21d0ef23b5ed13b7bb4
Daniil Dubovik <daniil.dubovik@gmail.com>
2024-03-14 17:58:00 +0300

    Added base functionality into `Presenter` class

--- Assets/MVP/Presenter.cs
+++ Assets/MVP/Presenter.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mvp
 {
-    public class Presenter<TView, TModel> : IPresenter
+    public class Presenter<TView, TModel> : IPresenter, IClearable
         where TView : IView
         where TModel : IModel
     {
@@ -9,12 +9,32 @@
 
         public Presenter()
         {
+            OnAddEventHandlers();
         }
 
         public Presenter(TView view, TModel model)
         {
             this.view = view;
             this.model = model;
+            OnAddEventHandlers();
+        }
+
+        public void Clear()
+        {
+            OnRemoveEventHandlers();
+            OnClear();
+        }
+
+        protected virtual void OnAddEventHandlers()
+        {
+        }
+
+        protected virtual void OnRemoveEventHandlers()
+        {
+        }
+
+        protected virtual void OnClear()
+        {
         }
     }
 }
